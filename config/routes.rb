13a919af46219e3ae114f6ab3e600b92d45b5ed2
Jonathan Pein <jonathan.o.k.pein@gmail.com>
2019-11-19 17:53:26 +0000

    added validations to bookings so that you can only book if dates have not been taken

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :garments do
-    resources :bookings, only: [:index, :new, :create, :destroy]
+    resources :bookings, only: [:new, :create, :destroy]
   end
+
+  # get 'bookings', to: 'bookings#index'
+  resources :bookings
+
   root to: 'garments#index'
 end
