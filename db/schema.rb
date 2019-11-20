7ac8fc21c1ccf2224d595e310fd424b9ccd8ecdb
Jonathan Pein <jonathan.o.k.pein@gmail.com>
2019-11-20 14:27:37 +0000

    added back-end to the profile page

--- db/schema.rb
+++ db/schema.rb
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 2019_11_20_132049) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
+    t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
