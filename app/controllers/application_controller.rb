1d8f8b09600b63a0fd489d7fdbc02daf1c381168
Jonathan Pein <jonathan.o.k.pein@gmail.com>
2019-11-20 16:03:49 +0000

    Profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,8 @@ class ApplicationController < ActionController::Base
 
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :avatar])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :avatar, :name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :avatar, :email])
   end
 
   def show
